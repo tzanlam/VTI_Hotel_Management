224c87bdf98c438e846209c4f6d128b72babb91b
tzanlam <tzanlam@gmail.com>
2025-02-15 08:56:57 +0700

    add query repo

--- hotel_management/src/main/java/hotel/hotel_management/repository/AccountRepository.java
+++ hotel_management/src/main/java/hotel/hotel_management/repository/AccountRepository.java
@@ -2,8 +2,16 @@ package hotel.hotel_management.repository;
 
 import hotel.hotel_management.modal.entity.hotel.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Integer> {
+    @Query("SELECT a FROM Account a JOIN a.hotels h WHERE h.id = :hotelId")
+    List<Account> findByHotelId(@Param("hotelId") int hotelId);
+
+    Account findByEmail(String email);
 }
