8c4a4b1961e6cb728dfbd91e0934334a5a3577a5
tzanlam <tzanlam@gmail.com>
2025-02-15 10:22:12 +0700

    login user

---
+++ hotel_management/src/main/java/hotel/hotel_management/config/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package hotel.hotel_management.config;
+
+import hotel.hotel_management.modal.entity.hotel.Account;
+import hotel.hotel_management.repository.AccountRepository;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final AccountRepository accountRepository;
+
+    public CustomUserDetailsService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Account account = accountRepository.findByEmail(email);
+        if (Objects.nonNull(account)) {
+            return new User(
+                    email,
+                    account.getPassword(),
+                    AuthorityUtils.createAuthorityList(account.getPosition().name())
+            );
+        }
+        throw new UsernameNotFoundException(email);
+    }
+}
