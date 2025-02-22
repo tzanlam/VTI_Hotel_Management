e3c5c1e0239588857d051f8b746994aa7a7f412d
tzanlam <tzanlam@gmail.com>
2025-02-22 10:49:20 +0700

    update response err auth

--- hotel_management/src/main/java/hotel/hotel_management/config/security/JwtAuthenticationPoint.java
+++ hotel_management/src/main/java/hotel/hotel_management/config/security/JwtAuthenticationPoint.java
@@ -1,7 +1,6 @@
 package hotel.hotel_management.config.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
@@ -15,14 +14,14 @@ import java.util.Map;
 @Component
 public class JwtAuthenticationPoint implements AuthenticationEntryPoint {
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         response.setContentType("application/json");
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
         final Map<String, Object> map = new HashMap<>();
         map.put("message", "Unauthorized");
         map.put("status", HttpServletResponse.SC_UNAUTHORIZED);
-        map.put("error", authException.getMessage());
+        map.put("error", "no authentication");
         map.put("path", request.getRequestURI());
 
         final ObjectMapper mapper = new ObjectMapper();
