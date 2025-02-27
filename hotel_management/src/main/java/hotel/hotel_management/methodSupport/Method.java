ae307d729a6cefe69baf3720c3a7a8b4a594bf6d
tzanlam <tzanlam@gmail.com>
2025-02-27 22:16:37 +0700

    add method convert

--- hotel_management/src/main/java/hotel/hotel_management/methodSupport/Method.java
+++ hotel_management/src/main/java/hotel/hotel_management/methodSupport/Method.java
@@ -1,5 +1,10 @@
 package hotel.hotel_management.methodSupport;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Random;
 
 public class Method {
@@ -13,5 +18,53 @@ public class Method {
         }
 
         return confirmationCode.toString();
+    }
+
+    public static <E extends Enum<E>> E convertStringToEnum(Class<E> enumClass, String string){
+        if (enumClass == null || string == null){
+            throw new NullPointerException();
+        }
+        try{
+            return Enum.valueOf(enumClass, string.toUpperCase());
+        }catch (IllegalArgumentException e){
+            throw new IllegalArgumentException(
+                    "Invalid enum value: " + enumClass.getSimpleName() + ": " + string
+            );
+        }
+    }
+
+    public static LocalDate convertToLocalDate(String dateString) throws Exception {
+        try {
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            return LocalDate.parse(dateString, dateFormatter);
+        } catch (DateTimeParseException e) {
+            throw new Exception("error formatter date: " + dateString);
+        }
+    }
+
+    public static LocalTime convertToLocalTime(String timeString) {
+        try {
+            DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+            return LocalTime.parse(timeString, timeFormatter);
+        } catch (DateTimeParseException e) {
+            System.out.println("error formatter time: " + timeString);
+            return null;
+        }
+    }
+
+//    public static LocalDateTime convertToLocalDateTime(String dateTimeString) {
+//        try {
+//            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+//            return LocalDateTime.parse(dateTimeString, dateTimeFormatter);
+//        } catch (DateTimeParseException e) {
+//            System.out.println("error formatter datetime");
+//            return null;
+//        }
+//    }
+
+    public static LocalDateTime buildLocalDateTime(String date, String time) {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        return LocalDateTime.of(LocalDate.parse(date, dateFormatter), LocalTime.parse(time, timeFormatter));
     }
 }
