a5b3c32cc1742651809cf48f082aa10ac990f049
tzanlam <tzanlam@gmail.com>
2025-03-04 22:38:16 +0700

    add query

--- hotel_management/src/main/java/hotel/hotel_management/repository/GuestRepository.java
+++ hotel_management/src/main/java/hotel/hotel_management/repository/GuestRepository.java
@@ -7,9 +7,13 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface GuestRepository extends JpaRepository<Guest, Integer> {
     @Query("select g from Guest g join Hotel h where h.id = :hotelId")
     List<Guest> findByHotelId(@Param("hotelId")int hotelId);
+
+    @Query("select g from Guest g where g.fullName = :fullName")
+    Optional<Guest> findByFullName(@Param("fullName")String fullName);
 }
